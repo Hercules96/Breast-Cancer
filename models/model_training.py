f67fe8d5bc60d512108619094f1fbfd9f9e87254
Abhinav Bhardwaj <abhinavbhardwaj835@gmail.com>
2025-11-08 20:49:32 +0530

    Importing the libraries and designing the random forest classifier

---
+++ models/model_training.py
@@ -0,0 +1,20 @@
+import pandas as pd
+from sklearn.datasets import load_breast_cancer
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score
+import joblib
+
+data = load_breast_cancer()
+X = pd.DataFrame(data.data, columns=data.feature_names)
+y = pd.Series(data.target)
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+model = RandomForestClassifier()
+model.fit(X_train, y_train)
+
+pred = model.predict(X_test)
+print("Accuracy:", accuracy_score(y_test, pred))
+
+joblib.dump(model, "breast_cancer_model.pkl")
